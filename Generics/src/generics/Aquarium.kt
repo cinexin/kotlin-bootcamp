9c6f87cdea6a4f7265e1eb88d92124971894712f
cinexin <migueguillamon@gmail.com>
2019-10-12 11:09:48 +0200

    Generics II

--- Generics/src/generics/Aquarium.kt
+++ Generics/src/generics/Aquarium.kt
@@ -17,7 +17,29 @@ class LakeWater : WaterSupply(true) {
 }
 
 
-class Aquarium<T: WaterSupply>(val waterSupply: T)
+class Aquarium<out T: WaterSupply>(val waterSupply: T) {
+    fun addWater() {
+        /**
+         * check is a Kotlin method that throws an IllegalStateException when condition is false
+         *
+         * ie: "check that water supply doesn't need processing"
+         */
+        check(!waterSupply.needsProcessing) {
+            "water supply needs processing first"
+        }
+        println ("Adding water from $waterSupply")
+    }
+
+    fun addWater(cleaner: Cleaner<T>) {
+        if (waterSupply.needsProcessing) {
+            cleaner.clean(waterSupply)
+        }
+        println("water added")
+    }
+
+    inline fun <reified R: WaterSupply> hasWaterSupplyOfType() = waterSupply is R
+}
+
 
 fun genericsExample() {
     val aquarium = Aquarium(TapWater())
@@ -36,4 +58,55 @@ fun genericsExample3() {
     val aquarium3 = Aquarium(LakeWater())
     println(aquarium3.waterSupply.javaClass)
 
+}
+
+fun genericsExample4() {
+    val aquarium4 = Aquarium(LakeWater())
+    aquarium4.waterSupply.filter()
+    aquarium4.addWater()
+}
+
+fun addItemTo(aquarium: Aquarium<WaterSupply>) = println("item added")
+
+fun genericsExample5() {
+    val aquarium = Aquarium(TapWater())
+    addItemTo(aquarium)
+}
+
+
+interface Cleaner<in T: WaterSupply> {
+    fun clean(waterSupply: T)
+}
+
+class TapWaterCleaner: Cleaner<TapWater> {
+    override fun clean(waterSupply: TapWater) = waterSupply.addChemicalCleaners()
+}
+
+fun genericsExample6() {
+    val cleaner = TapWaterCleaner()
+    val aquarium = Aquarium(TapWater())
+    aquarium.addWater(cleaner)
+}
+
+fun <T:WaterSupply> isWaterClean(aquarium: Aquarium<T>) {
+    println ("Aquarium water is clean?: ${!aquarium.waterSupply.needsProcessing}")
+}
+
+fun genericsExample7() {
+    val aquarium = Aquarium(TapWater())
+    isWaterClean(aquarium)
+}
+
+inline fun <reified T: WaterSupply> WaterSupply.isOfType() = this is T
+
+fun genericsExample8() {
+    val aquarium = Aquarium(TapWater())
+    println("aquarium.waterSupply is of type TapWater?: " + aquarium.waterSupply.isOfType<TapWater>())
+}
+
+inline fun <reified R: WaterSupply> Aquarium<*>.hasWaterSupplyOfType() = waterSupply is R
+
+fun genericsExample9() {
+    val aquarium = Aquarium(TapWater())
+    println("aquarium.hasWaterSupplyOfType<TapWater>? " + aquarium.hasWaterSupplyOfType<TapWater>())
 }
--- Generics/src/generics/Main.kt
+++ Generics/src/generics/Main.kt
@@ -4,4 +4,10 @@ fun main() {
     genericsExample()
     genericsExample2()
     genericsExample3()
+    genericsExample4()
+    genericsExample5()
+    genericsExample6()
+    genericsExample7()
+    genericsExample8()
+    genericsExample9()
 }
